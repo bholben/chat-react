4d8d8c5271fb3aefc69b787e2617956afdcb373e
Bob Holben <bholben@tillster.com>
2017-05-23 22:20:56 -0400

    Pull all state out to new chat room container to prep for full chat encapsulation

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,89 +1,20 @@
-import React, { Component } from 'react';
-import { api, firebase } from 'api';
-import { hasEnter } from '../../utils/strings';
+import React from 'react';
 import Header from './Header';
 import Messages from './Messages';
 import MessageInput from './MessageInput';
 
-class Chat extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {},
-      displayName: 'Anonymous',
-      messageText: '',
-      messages: [],
-    };
-    this.changeMessageText = this.changeMessageText.bind(this);
-    this.sendMessage = this.sendMessage.bind(this);
-    this.deleteMessage = this.deleteMessage.bind(this);
-  }
-
-  componentDidMount() {
-    firebase.auth().signInAnonymously()
-      .then(user => {
-        // This is where we can spoof to get into another conversation
-        // user = {
-        //   uid: 'jc4JByFVHhhamVCDbQg2hj4mzEz2',
-        //   isAnonymous: true,
-        // }
-
-        api.syncMessages(user, messages => {
-          this.setState({ user, messages })
-        });
-      });
-  }
-
-  changeMessageText(e) {
-    const messageText = e.target.value;
-    if (hasEnter(messageText)) {
-      this.sendMessage(e);
-    } else {
-      this.setState({ messageText });
-    }
-  }
-
-  sendMessage(e) {
-    const message = {
-      displayName: this.state.displayName,
-      timestamp: firebase.database.ServerValue.TIMESTAMP,
-      text: this.state.messageText
-    };
-    this.enableAgentEasterEgg(message);
-    e.preventDefault();
-
-    api.sendMessage(message, this.state.user)
-      .then(() => this.setState({ messageText: '' }))
-      .catch(console.error);
-  }
-
-  deleteMessage(message) {
-    api.deleteMessage(message, this.state.user)
-      .catch(console.error);
-  }
-
-  enableAgentEasterEgg(message) {
-    // This is how we create texts as the agent on the left side
-    if (this.state.messageText.startsWith('//')) {
-      message.text = message.text.substring(2).trim();
-      message.displayName = 'Addison';
-      message.isAgent = true;
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Messages messages={this.state.messages}
-            deleteMessage={this.deleteMessage} />
-        <MessageInput
-            sendMessage={this.sendMessage}
-            changeMessageText={this.changeMessageText}
-            messageText={this.state.messageText} />
-      </div>
-    );
-  }
+function Chat (props) {
+  return (
+    <div>
+      <Header />
+      <Messages messages={props.messages}
+          deleteMessage={props.deleteMessage} />
+      <MessageInput
+          messageText={props.messageText}
+          changeMessageText={props.changeMessageText}
+          sendMessage={props.sendMessage} />
+    </div>
+  );
 }
 
 export default Chat;
--- src/containers/ChatRoom.js
+++ src/containers/ChatRoom.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { api, firebase } from 'api';
+import { hasEnter } from '../utils/strings';
 import Chat from '../components/Chat';
 
 class ChatRoom extends Component {
@@ -6,10 +8,65 @@ class ChatRoom extends Component {
     super(props);
     this.state = {
       user: {},
+      messages: [],
       displayName: 'Anonymous',
       messageText: '',
-      messages: [],
     };
+    this.changeMessageText = this.changeMessageText.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.deleteMessage = this.deleteMessage.bind(this);
+  }
+
+  componentDidMount() {
+    firebase.auth().signInAnonymously()
+      .then(user => {
+        // This is where we can spoof to get into another conversation
+        // user = {
+        //   uid: 'jc4JByFVHhhamVCDbQg2hj4mzEz2',
+        //   isAnonymous: true,
+        // }
+
+        api.syncMessages(user, messages => {
+          this.setState({ user, messages })
+        });
+      });
+  }
+
+  changeMessageText(e) {
+    const messageText = e.target.value;
+    if (hasEnter(messageText)) {
+      this.sendMessage(e);
+    } else {
+      this.setState({ messageText });
+    }
+  }
+
+  sendMessage(e) {
+    const message = {
+      displayName: this.state.displayName,
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      text: this.state.messageText
+    };
+    this.enableAgentEasterEgg(message);
+    e.preventDefault();
+
+    api.sendMessage(message, this.state.user)
+      .then(() => this.setState({ messageText: '' }))
+      .catch(console.error);
+  }
+
+  deleteMessage(message) {
+    api.deleteMessage(message, this.state.user)
+      .catch(console.error);
+  }
+
+  enableAgentEasterEgg(message) {
+    // This is how we create texts as the agent on the left side
+    if (this.state.messageText.startsWith('//')) {
+      message.text = message.text.substring(2).trim();
+      message.displayName = 'Addison';
+      message.isAgent = true;
+    }
   }
 
   render() {
@@ -17,10 +74,12 @@ class ChatRoom extends Component {
       <Chat user={this.state.user}
           displayName={this.state.displayName}
           messageText={this.state.messageText}
-          messages={this.state.messages} />
+          messages={this.state.messages}
+          changeMessageText={this.changeMessageText}
+          sendMessage={this.sendMessage}
+          deleteMessage={this.deleteMessage} />
     );
   }
-
 }
 
 export default ChatRoom;
